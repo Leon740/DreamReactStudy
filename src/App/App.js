3c20907a6c961e604249ceb44011cc53c8cb51d5
Leonid Dobrinov <leonid.dobrinov@geeksforless.com>
2022-05-12 14:15:31 +0300

    feat(App): structure

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import DreamCar from '../Car/DreamCar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Going good</h1>
+      <DreamCar />
+    </div>
+  );
+}
+
+export default App;
