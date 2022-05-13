258abdd71bb1193ac366ea18e88574ef6f810242
Leonid Dobrinov <leonid.dobrinov@geeksforless.com>
2022-05-13 14:26:12 +0300

    feat(subjects/ErrorBoundary)

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.scss";
-// import Car from "../subjects/Context/FromParentToChild/Car";
-import Car from "../subjects/Context/FromChildToParent/Car";
+import Car from "../subjects/Lazy/Car";
+import ErrorBoundary from "../subjects/ErrorBoundary/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
       <h1>Going good</h1>
-      <Car />
+      <ErrorBoundary>
+        <Car />
+      </ErrorBoundary>
     </div>
   );
 }
---
+++ src/subjects/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundary extends React.Component {
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // logErrorToMyServices(error, errorInfo);
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p style={{ color: "red" }}>Error</p>;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node,
+};
+
+export default ErrorBoundary;
