5b1961e1e9b493f01b7aa62b12ecbbff3f4fd6db
Leonid Dobrinov <leonid.dobrinov@geeksforless.com>
2022-05-28 00:17:47 +0300

    feat(subjects): StrictMode

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,10 @@
 import React, { StrictMode } from "react";
 import "./App.scss";
-import Car from "subjects/10_strictMode/Car";
 
 function App() {
   return (
     <div className="App container">
-      <StrictMode>
-        <Car />
-      </StrictMode>
+      <StrictMode />
     </div>
   );
 }
